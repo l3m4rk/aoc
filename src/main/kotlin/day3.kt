7933bece861495275c604fd281b1f04ccedcd31e
Dmitry <b4r4n0v@gmail.com>
2022-12-03 13:34:15 +0200

    feat: day 3 part 2

--- src/main/kotlin/day3.kt
+++ src/main/kotlin/day3.kt
@@ -11,28 +11,37 @@ object Day3 : Solution<List<String>> {
         super.run()
 
         readInput(day)
-            .fold(0) { sumOfPriorities: Int, rucksack: String ->
-                println("=====")
-                val part1 = rucksack.take(rucksack.length / 2).toList()
-                val part2 = rucksack.takeLast(rucksack.length / 2).toList()
-                println("Part1: $part1")
-                println("Part2: $part2")
-                val common = part1.intersect(part2).first()
-                println("common element: $common")
-                val itemPriority = priorities.indexOf(common) + 1
-                println("item priority: $itemPriority")
-                sumOfPriorities + itemPriority
+            .also { input ->
+                val result1 = part1(input)
+                println("part 1 result: $result1")
+                val result2 = part2(input)
+                println("part 2 result: $result2")
             }
-            .also { println(it) }
     }
 
-    override fun part1(input: List<String>): Int {
-        TODO("Not yet implemented")
-    }
+    override fun part1(input: List<String>): Int =
+        input.fold(0) { sumOfPriorities: Int, rucksack: String ->
+            println("=====")
+            val part1 = rucksack.take(rucksack.length / 2).toList()
+            val part2 = rucksack.takeLast(rucksack.length / 2).toList()
+            println("Part1: $part1")
+            println("Part2: $part2")
+            val common = part1.intersect(part2).first()
+            println("common element: $common")
+            val itemPriority = priorityOf(common)
+            println("item priority: $itemPriority")
+            sumOfPriorities + itemPriority
+        }
+
+    private fun priorityOf(common: Char) = priorities.indexOf(common) + 1
 
     override fun part2(input: List<String>): Int {
-        TODO("Not yet implemented")
+        return input
+            .chunked(3)
+            .fold(0) { sumOfPriorities, group ->
+                val (first, second, third) = group.map { it.toSet() }
+                val common = third.intersect(first.intersect(second)).first()
+                sumOfPriorities + priorityOf(common)
+            }
     }
-
-
 }
